7707a1b4f0a15db01044c68906729650b62004dc
Krishna Kanani <kananikrishna427@gmail.com>
2023-09-10 12:19:25 +0530

    Assignment-2 : Print string in reverse

--- src/technoCredits/Assignment2.java
+++ src/technoCredits/Assignment2.java
@@ -8,8 +8,16 @@ public class Assignment2 {
 		}
 	}
 	
+	void printLengthOfGivenStringInRev(String input) {
+		for(int index=input.length()-1;index>=0;index--) {		
+			System.out.println(index + "->" + input.charAt(index));
+		}
+	}
+	
 	public static void main(String[] args) {
 		Assignment2 assignment2 = new Assignment2();
 		assignment2.printLengthOfGivenString("technocredits");
+		System.out.println("===================");
+		assignment2.printLengthOfGivenStringInRev("technocredits");
 	}
 }
