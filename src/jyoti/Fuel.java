aa527c42834131e7885ba7d522d54430af66c77c
Jyoti Saxena <jyotisaxena2006@gmail.com>
2023-09-10 10:26:36 +0530

    Jyoti_Prac_Fuel

---
+++ src/jyoti/Fuel.java
@@ -0,0 +1,35 @@
+package jyoti;
+class Fuel{
+
+	int topup=500;
+	int balance;
+	int totalTopup;
+	void fillFuel(int topup){ //parameter hold the value
+	System.out.println("Sir, fuel is added worth rs" + topup);
+	totalTopup=totalTopup + topup;
+	}
+	void add(int num1,int num2,int num3){
+		
+	}
+	void credit(int amt){
+		balance=balance + amt;
+	}
+	
+	
+	void debit(int amt){
+		balance=balance - amt;
+	}
+	
+	void display(){
+		System.out.println("So far , you filled" + totalTopup);
+		//System.out.println(balance);
+	}
+	public static void main(String[] args){
+	Fuel fuel= new Fuel();
+	fuel.fillFuel(350);
+	fuel.fillFuel(850);
+	fuel.fillFuel(3500);
+	
+	fuel.add(20,40,8);
+	}
+}
