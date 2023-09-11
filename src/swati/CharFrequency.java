e66ea2b85ec534ad22108f19fde17e1b53bab483
Swati Kudale <swatikudale92@gmail.com>
2023-09-11 12:59:14 +0530

    Swati_Assignment19_Print_reverse_order

---
+++ src/swati/CharFrequency.java
@@ -0,0 +1,24 @@
+/*Assignment-19 : 10th Sep'2023
+ *  1. Print the frequency of given character from given String. input : technocredits       
+t output : frequency of t in technocredits is 2.*/
+
+package swati;
+
+public class CharFrequency {
+	
+	void printChar(String input,char ch) {
+		int count = 0;
+		for(int index=0;index<=input.length()-1;index++) {
+			if(input.charAt(index) == ch) {
+			count++;
+			}
+		}		
+		System.out.println("frequency of "+ch+" in technocredits is "+count);
+	}
+
+	public static void main(String[] args) {
+		CharFrequency charFrequency = new CharFrequency();
+		charFrequency.printChar("technocredits",'t');
+	}
+
+}
