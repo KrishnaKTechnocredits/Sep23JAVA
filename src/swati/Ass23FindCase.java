2233714f9556a553787a2ec96d03c9f81f7581b1
Swati Kudale <swatikudale92@gmail.com>
2023-09-15 12:20:21 +0530

    Swati_Assignment23_FindCountOfChar

--- src/swati/Ass23FindCase.java
+++ src/swati/Ass23FindCase.java
@@ -1,24 +1,31 @@
+/*Assignment - 23 : 14th Sep'2023 Write a program to find count of Uppercase, lowercase, digits and special characters. String str = "TecHNo_cR6ediTs"; output : 
+Uppercase -> 5  
+Lowercase -> 8 
+Digit -> 1 Special
+char count -> 1*/
+
 package swati;
 
 public class Ass23FindCase {
 	int uppercasecount = 0;
-	int lowercasecount =0 ;
+	int lowercasecount = 0;
 	int digitcount = 0;
 	int specialcount = 0;
-	
+
 	void findCharCase(String input) {
-		for(int index=0;index<input.length();index++) {
-			if(Character.isUpperCase(input.charAt(index))) {
+		for (int index = 0; index < input.length(); index++) {
+			if (Character.isUpperCase(input.charAt(index))) {
 				uppercasecount++;
-			}else if(Character.isLowerCase(input.charAt(index))) {
+			} else if (Character.isLowerCase(input.charAt(index))) {
 				lowercasecount++;
-			}else if(Character.isDigit(input.charAt(index))) {
+			} else if (Character.isDigit(input.charAt(index))) {
 				digitcount++;
-			}else {
+			} else {
 				specialcount++;
-			} 
+			}
 		}
-		System.out.println("uppercase-> "+uppercasecount+" Lowercase-> "+lowercasecount+ " Digit-> "+digitcount+ " Special char count-> "+specialcount);;
+		System.out.println("uppercase-> " + uppercasecount + "\nLowercase-> " + lowercasecount + "\nDigit-> "
+				+ digitcount + "\nSpecial char count-> " + specialcount);
 	}
 
 	public static void main(String[] args) {
