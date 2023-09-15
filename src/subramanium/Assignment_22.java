e3075c8d41446c02e555d88ba567e6f43e8c7cf0
Subramanian Pillai <subramanian.pillai@hotmail.com>
2023-09-15 15:49:38 +0530

    Assignment_22 after indentation correction

--- src/subramanium/Assignment_22.java
+++ src/subramanium/Assignment_22.java
@@ -1,25 +1,25 @@
 package subramanium;
 
 public class Assignment_22 {
-	
+
 	void uniqueChar(String input) {
-		for(int index=0;index<input.length();index++){
+		for (int index = 0; index < input.length(); index++) {
 			char ch = input.charAt(index);
-		
-		if(input.indexOf(ch)==input.lastIndexOf(ch)) {
-			System.out.print(ch);
-		}
+
+			if (input.indexOf(ch) == input.lastIndexOf(ch)) {
+				System.out.print(ch);
+			}
 		}
 	}
 
 	public static void main(String[] args) {
 		Assignment_22 assignment_22 = new Assignment_22();
-		String input="technocredit";
+		String input = "technocredit";
 		assignment_22.uniqueChar(input);
 		System.out.println();
 		System.out.println("==================");
-		System.out.println("First Index of t in "+input+" is "+input.indexOf('t'));
-		System.out.println("Last Index of t in "+input+" is "+input.lastIndexOf('t'));
+		System.out.println("First Index of t in " + input + " is " + input.indexOf('t'));
+		System.out.println("Last Index of t in " + input + " is " + input.lastIndexOf('t'));
 	}
 
 }
