61937e9b2df0ef82a5142d27a08f8947f84a1efa
Jyoti Saxena <jyotisaxena2006@gmail.com>
2023-09-17 01:24:33 +0530

    Jyoti_Assignment23

---
+++ src/jyoti/Assignment23.java
@@ -0,0 +1,45 @@
+/*
+Assignment - 23 : 14th Sep'2023
+Write a program to find count of Uppercase, lowercase, digits and special characters.
+String str = "TecHNo_cR6ediTs";
+output : Uppercase -> 5
+         Lowercase -> 8
+		 Digit -> 1
+		 Special char count -> 1
+*/
+package jyoti;
+public class Assignment23{
+
+
+	void stringCheck(String input){
+		int uCount=0;
+		int lCount=0;
+		int dCount=0;
+		int spaceCount=0;
+		int sCount=0;
+
+		for (int index=0;index<input.length();index++){
+			if (Character.isUpperCase(input.charAt(index))){
+				uCount++;
+				}
+				else if(Character.isLowerCase(input.charAt(index))){
+					lCount++;
+				}
+				else if(Character.isDigit(input.charAt(index))){
+				dCount++;
+				}
+				else if(input.charAt(index)== ' '){
+			spaceCount++;		
+			}
+			else{
+			sCount++;
+			}
+	}
+	System.out.println("Lowercase count-->"+lCount+'\n'+"Uppercase Count-->"+uCount+'\n'+"Digit--->"+dCount+'\n'+"Space counts-->"+spaceCount+'\n'+"SpecialCharacterCount--->"+sCount);	
+}
+
+public static void main(String[] args){
+Assignment23 strcs = new Assignment23();
+strcs.stringCheck("TecHN$ocR  1ediTs");
+	}
+}
