2227aa514c4fea415adf4a36ccecb78dcba1485d
Jyoti Saxena <jyotisaxena2006@gmail.com>
2023-09-18 19:24:10 +0530

    Jyoti_Assignment28

---
+++ src/jyoti/Assignment28.java
@@ -0,0 +1,28 @@
+package jyoti;
+
+/*Assingment - 28 : 18th Sep'2023
+Print the first non repeatative character using while loop.
+String str = "technocredits";
+output : h*/
+
+class Assignment28{
+
+void print(String str){
+
+		while(str.length()>0){
+		int orgLen=str.length();
+		char ch= str.charAt(0);
+		str=str.replace(String.valueOf(ch),"");
+		int newLen=str.length();
+			if(orgLen-newLen == 1){
+			System.out.println("First non repeatative character-->"+ch);
+			break;
+			}
+		}
+
+	}
+	public static void main(String[] args){
+	Assignment28 fnwl= new Assignment28();
+	fnwl.print("technocredits");
+	}
+}
