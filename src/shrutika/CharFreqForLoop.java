ca81991c877dcfe513345e2c2788960430c0d912
Shrutika Benakatti <shrutikad905@gmail.com>
2023-09-19 17:06:06 +0530

    Shrutika_Assignment_25

---
+++ src/shrutika/CharFreqForLoop.java
@@ -0,0 +1,33 @@
+/*Assingment - 25 : 18th Sep'2023
+Print the freq of all character using nested for loop.
+String str = "aakanksha";
+output : a -> 4
+         k -> 2
+		 n -> 1 
+		 s -> 1
+         h -> 1*/
+package shrutika;
+
+public class CharFreqForLoop {
+
+	void charFreq(String input) {
+		for (int index = 0; index < input.length(); index++) {
+			int count = 0;
+			char ch = input.charAt(index);
+			if (ch != '*') {
+				for (int innerIndex = 0; innerIndex < input.length(); innerIndex++) {
+					if (ch == input.charAt(innerIndex)) {
+						count++;
+					}
+				}
+				input = input.replace(ch, '*');
+				System.out.println(ch + " -> " + count);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		CharFreqForLoop charFreqForLoop = new CharFreqForLoop();
+		charFreqForLoop.charFreq("aakanksha");
+	}
+}
