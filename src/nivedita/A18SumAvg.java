bf49bb0dbea606baacb414ca235cd619f1abc7ae
Nivedita Karnawat <karnawatnivi.com>
2023-09-23 12:30:18 +0530

    Nivedita_Assignment_17_19

--- src/nivedita/A18SumAvg.java
+++ src/nivedita/A18SumAvg.java
@@ -1,33 +1,38 @@
-/* Assignment 18
-On user defined range print sum and average of numbers which is divisible by 5 & 7.
-Hint : pass start and end index as a parameter.
-Input : Range -> 1 to 110
-Output : Numbers are,
-                35
-		 70
-                105
-		 Found 3 numbers which are divible by 5 & 7.
-		 sum = 210
-		 Avearge = 70 
-*/
+/* Assignment-18 : 8th Sep'2023 On user defined range print sum and average of numbers which is divisible by 5 & 7. Hint : pass start and end index as a parameter. 
+Input : Range -> 1 to 110 Output :
+     Numbers are,           
+     35 70 105 
+	 Found 3 numbers which are divible by 5 & 7.
+	 sum = 210
+	 Avearge = 70  */
 
-package nivedita;
+package nivedita;	 
 class A18SumAvg{
-	void proccessData1(int startRange, int endRange){
-		int count = 0;
-		int sum = 0;
-		for(int num=endRange;num>=startRange;num--){
-			if(num%5==0 && num%7==0){
-				sum = sum + num;
-				count++;
+	
+	void divisibleNumb(int StartRange,int EndRange){
+		
+		int sum=0;
+		int average=0;
+		int total=0;
+		
+		System.out.println("Numbers are, ");
+		for(int number=StartRange;number<=EndRange;number++){
+			if(number%5==0 && number%7==0){
+				System.out.println(" "+number);
+				total++;
+				sum=sum+number;
 			}
 		}
-		System.out.println(sum);
+		if(sum!=0 && total!=0){
+			average=sum/total;
+		}
+		System.out.println("Found "+total+" numbers which are divisible by 5 & 7");
+		System.out.println("sum = "+sum);
+		System.out.println("Average = "+average);
 	}
 	
 	public static void main(String[] args){
-		A17DivisibleNum divisibleNum = new A17DivisibleNum();
-		System.out.println("The numbers divisible by 5 and 7 are : ");
-		divisibleNum.proccessData(1,100);
-		}
-}		 
+		A18SumAvg sumAvg = new A18SumAvg();
+		sumAvg.divisibleNumb(1,110);
+	}
+}	 
