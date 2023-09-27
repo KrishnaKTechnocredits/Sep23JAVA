e82173af4e13645fc71846cdc75eb8344eadf044
Nivedita Karnawat <karnawatnivi.com>
2023-09-27 18:28:34 +0530

    Nivedita_Programming_Test_1

---
+++ src/nivedita_test/Nivedia_Program_test_1.java
@@ -0,0 +1,47 @@
+package nivedita_test;
+
+public class Nivedia_Program_test_1 {
+	String getOutput(String input) {
+		String digitOutput = "";
+		String smallLetter = "";
+		String capitalLetter = "";
+
+		for (int index = 0; index < input.length(); index++) {
+			char ch = input.charAt(index);
+			if (Character.isDigit(ch)) {
+				digitOutput = digitOutput + ch;
+			} else if (Character.isLowerCase(ch)) {
+				smallLetter = smallLetter + ch;
+			} else if (Character.isUpperCase(ch)) {
+				capitalLetter = capitalLetter + ch;
+			}
+		}
+		String output = digitOutput + smallLetter + capitalLetter;
+		return output;
+	}
+
+	String getOutput1(String input) {
+		int digitSum = 0;
+		String smallLetter = "";
+		String capitalLetter = "";
+
+		for (int index = 0; index < input.length(); index++) {
+			char ch = input.charAt(index);
+			if (Character.isDigit(ch)) {
+				digitSum = digitSum + Character.getNumericValue(ch);
+			} else if (Character.isLowerCase(ch)) {
+				smallLetter = smallLetter + ch;
+			} else if (Character.isUpperCase(ch)) {
+				capitalLetter = capitalLetter + ch;
+			}
+		}
+		String output = digitSum + smallLetter + capitalLetter;
+		return output;
+	}
+
+	public static void main(String[] args) {
+		Nivedia_Program_test_1 test_1 = new Nivedia_Program_test_1();
+		System.out.println(test_1.getOutput("TeCHNoc33r44editS"));
+		System.out.println(test_1.getOutput1("TeCHNoc33r44editS"));
+	}
+}
