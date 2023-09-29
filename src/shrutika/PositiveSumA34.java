2ba024ee09ff0ecff42ed800487d0ab89363c2ae
Shrutika Benakatti <shrutikad905@gmail.com>
2023-09-29 22:49:42 +0530

    Shrutika_Assignment_34

---
+++ src/shrutika/PositiveSumA34.java
@@ -0,0 +1,34 @@
+/*Assignment-34 :  26th Sep'2023
+Return positive difference of sum of all the even numbers and odd numbers from given array.
+input : {10,3,44,33,35}
+output : 17 */
+package shrutika;
+
+public class PositiveSumA34 {
+
+	int getSumOfNum(int[] arrInput) {
+		int evenSum = 0;
+		int oddSum = 0;
+		int diffSum = 0;
+		for (int index = 0; index < arrInput.length; index++) {
+			if (arrInput[index] % 2 == 0) {
+				evenSum = evenSum + arrInput[index];
+			} else {
+				oddSum = oddSum + arrInput[index];
+			}
+			if (evenSum > oddSum) {
+				diffSum = evenSum - oddSum;
+			} else {
+				diffSum = oddSum - evenSum;
+			}
+		}
+		return diffSum;
+	}
+
+	public static void main(String[] args) {
+		int[] input = { 10, 3, 44, 33, 35 };
+		PositiveSumA34 positiveSumA34 = new PositiveSumA34();
+		int output = positiveSumA34.getSumOfNum(input);
+		System.out.println(output);
+	}
+}
