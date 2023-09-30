9cb39cf625ff31a39dea82b73d861fda4994a49b
Hetalvirani06 <hetalusadadiya@gmail.com>
2023-09-30 07:31:39 -0700

    Hetal_Asiignment_37

---
+++ src/hetal/Case3.java
@@ -0,0 +1,13 @@
+package hetal;
+
+public class Case3 {
+	public static void main(String[] args) {
+		A a = new B();
+		System.out.println(a.x);
+		System.out.println(a.y);
+		//System.out.println(a.z);
+		a.m1();
+		a.m2();
+		//a.m3();
+	}
+}
