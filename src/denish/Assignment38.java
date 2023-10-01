ecd47a9cd609ab1057933a2d2d990d3e9241a4bd
Denish Santoki <denishsantoki74@.com>
2023-10-01 16:52:58 +0530

    Denish_Assignment_38

---
+++ src/denish/Assignment38.java
@@ -0,0 +1,34 @@
+package denish;
+
+public class Assignment38 {
+	
+	int sumOfDigit(String input) {
+		boolean flag;
+		int sum=0;
+		
+		for(int index=0; index<input.length(); index++) {
+			char ch = input.charAt(index);
+			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+				flag=true;
+				break;
+			}
+		}
+		
+		if(flag=true) {
+			for(int index=0; index<input.length();index++) {
+				char ch = input.charAt(index);
+				if(Character.isDigit(ch)) {
+					int x = Character.getNumericValue(ch);
+					sum = sum + x;
+				}
+			}
+		}
+		System.out.println(sum);
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		Assignment38 assignment38 = new Assignment38();
+		assignment38.sumOfDigit("Pu1ne5Ind3ia");
+	}
+}
