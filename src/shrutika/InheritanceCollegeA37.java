dba3a0e28a58d21f8e27e9fd1bf5afb0327a05eb
Shrutika Benakatti <shrutikad905@gmail.com>
2023-10-02 11:34:39 +0530

    Shrutika_Assignment_37

---
+++ src/shrutika/InheritanceCollegeA37.java
@@ -0,0 +1,14 @@
+package shrutika;
+
+public class InheritanceCollegeA37 {
+
+	int rollno = 10;
+	int registerNo = 20;
+	
+	void registeration() {
+		System.out.println("registeration");
+	}
+	void admission() {
+		System.out.print("admission");
+	}
+}
---
+++ src/shrutika/InheritanceStudentA37.java
@@ -0,0 +1,14 @@
+package shrutika;
+
+public class InheritanceStudentA37 extends InheritanceCollegeA37 {
+
+	int studentId = 30;
+	int registerNo = 40;
+	
+	void registeration() {
+		System.out.println("registeration");
+	}
+	void marks() {
+		System.out.print("marks");
+	}
+}
