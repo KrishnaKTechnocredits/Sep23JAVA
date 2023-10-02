90669f3ebe5468f6c3658dc12fcc55e174608aaa
Sayali Honrao <sayali.honrao@gmail.com>
2023-10-02 22:54:20 +0530

    Sayali_Assignment_41

---
+++ src/sayali/Assignment41.java
@@ -0,0 +1,13 @@
+/* WAP to print a string from 1st vowel
+Input: pqrabcgtyes
+Output: abcgtyes */
+
+package sayali;
+
+class Assignment41 {
+
+	public static void main(String[] args) {
+		String str = "pqrabcgtyes";
+		System.out.println("Output: " + str.substring(3));
+	}
+}
