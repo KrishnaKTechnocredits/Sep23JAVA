aab122e4c444856ae50839d054dc5290e9ebc80b
Rahul Hiremath <hrahul34@yahoo.com>
2023-10-03 00:05:48 +0530

    Rahul_Assignment-29

---
+++ src/rahul/Assignment29_1.java
@@ -0,0 +1,18 @@
+/*Assingment - 29 : 18th Sep'2023
+Print the last non repeatative character using while loop.
+String str = "technocredits";
+output : h*/
+
+package rahul;
+
+public class Assignment29_1 {
+
+	void m1() {
+
+	}
+
+	public static void main(String[] args) {
+		Assignment29_1 assignment29_1 = new Assignment29_1();
+		assignment29_1.m1();
+	}
+}
