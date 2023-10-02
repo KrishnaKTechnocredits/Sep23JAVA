aaa3ebeca347ddd629aad96d43418c68c91f96d8
akshitakadu24 <akshita.kadu24@gmail.com>
2023-10-02 10:08:43 +0530

    Akshita_Assignment_38_Indentation updated

--- src/akshita/Vowels.java
+++ src/akshita/Vowels.java
@@ -5,35 +5,32 @@
 package akshita;
 
 public class Vowels {
-	boolean test;
-	int sum=0;
 	int sum(String input) {
-		
-		for(int index=0;index<input.length();index++){
+		boolean test = false;
+		int sum = 0;
+		for (int index = 0; index < input.length(); index++) {
 			char ch = input.charAt(index);
-			if(ch =='a'|| ch=='e' || ch=='i' || ch=='o' || ch=='u') {
-				test = true;	
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				test = true;
 			}
 		}
-		if(test==true) {
-			for(int index=0;index<input.length();index++){
+		if (test == true) {
+			for (int index = 0; index < input.length(); index++) {
 				char ch = input.charAt(index);
-				if(Character.isDigit(ch)) {
-					sum= sum + Character.getNumericValue(ch); 
-				}	
-			}	
+				if (Character.isDigit(ch)) {
+					sum = sum + Character.getNumericValue(ch);
+				}
+			}
 		}
 		return sum;
-	}	
-public static void main(String[] args) {
+	}
+
+	public static void main(String[] args) {
 		System.out.println("Print sum of digit if there is any vowels in string : ");
 		Vowels vowels = new Vowels();
 		String v1 = "Pu1ne5lnd3ia";
-		System.out.println("Input : " + v1 +" => " + "Ouput : " + vowels.sum(v1));
-		Vowels s2 = new Vowels();
+		System.out.println("Input : " + v1 + " => " + "Ouput : " + vowels.sum(v1));
 		String v2 = "R1hlTns7h";
-		System.out.println("Input : " + v2 +"   => " + "Output : " + s2.sum(v2));	
-		
-		
+		System.out.println("Input : " + v2 + "   => " + "Output : " + vowels.sum(v2));
 	}
 }
