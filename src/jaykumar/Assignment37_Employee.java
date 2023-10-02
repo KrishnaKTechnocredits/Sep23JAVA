5057071205be3ce6bb9c5524e8f94897a1f9d5c2
Jaykumar Bankar <bankarjaykumar@gmail.com>
2023-10-02 18:51:24 +0530

    Jaykumar_Assignment37

---
+++ src/jaykumar/Assignment37_Employee.java
@@ -0,0 +1,14 @@
+package jaykumar;
+
+public class Assignment37_Employee extends Assignment37_Manager {
+	int y = 30;
+	int z = 40;
+	
+	void m2() {
+		System.out.println("Employee-m2");
+	}
+	
+	void m3() {
+		System.out.println("Employee-m3");
+	}
+}
