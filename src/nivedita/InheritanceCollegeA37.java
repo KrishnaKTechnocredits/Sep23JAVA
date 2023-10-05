ff9737b54cfeb3f8564545912491a21d540beb8e
Nivedita Karnawat <karnawatnivi.com>
2023-10-05 16:06:19 +0530

    Nivedita_Assignment_37

---
+++ src/nivedita/InheritanceCollegeA37.java
@@ -0,0 +1,14 @@
+package nivedita;
+
+public class InheritanceCollegeA37 {
+
+	int rollno = 16;
+	int registerNo = 40;
+
+	void registeration() {
+		System.out.println("Registration");
+	}
+	void admission() {
+		System.out.print("Admission");
+	}
+}
---
+++ src/nivedita/InheritanceStudentA37.java
@@ -0,0 +1,14 @@
+package nivedita;
+
+public class InheritanceStudentA37 extends InheritanceCollegeA37 {
+
+	int studentId = 90;
+	int registerNo = 46;
+
+	void registeration() {
+		System.out.println("Registration");
+	}
+	void marks() {
+		System.out.print("Marks");
+	}
+}
