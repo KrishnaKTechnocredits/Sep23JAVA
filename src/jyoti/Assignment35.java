163ba8d911ccaa0ed1de9f4c45685c7f56bf1111
Jyoti Saxena <jyotisaxena2006@gmail.com>
2023-10-07 00:52:33 +0530

    Jyoti_Assignment35

---
+++ src/jyoti/Assignment35.java
@@ -0,0 +1,30 @@
+/*Assignment-35 :  26th Sep'2023
+Print name and its first letter from given array.
+input :  { "Sayli", "deepak", "Nivedita", "Yogesh" }
+output : Sayli -> S
+         deepak -> d
+         Nivedita -> N
+		 Yogesh -> Y
+
+*/
+
+package jyoti;
+
+public class Assignment35 {
+
+	void printStartLetter(String[] arr) {
+
+		for (int index = 0; index < arr.length; index++) {
+
+			System.out.println(arr[index] + "--->" + arr[index].charAt(0));
+		}
+	}
+
+	public static void main(String[] aregs) {
+		String[] arr = { "Sayli", "deepak", "Nivedita", "Yogesh" };
+		Assignment35 as = new Assignment35();
+		as.printStartLetter(arr);
+
+	}
+
+}
