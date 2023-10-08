2b735580a65034fb5d3b95b758ac33f82259a37f
harshvegada <harshhpatel07@gmail.com>
2023-10-08 08:19:25 +0530

    CE removed

---
+++ src/technoCredits/commBwTwoClass/A.java
@@ -0,0 +1,17 @@
+package technoCredits.commBwTwoClass;
+
+public class A {
+
+	void m1() {
+		System.out.println("A class m1");
+	}
+
+	void m2() {
+		System.out.println("A class m2");
+	}
+
+	static void m3() {
+		System.out.println("A class m3");
+	}
+
+}
---
+++ src/technoCredits/dataTypes/DataTypeExample.java
@@ -0,0 +1,19 @@
+package technoCredits.dataTypes;
+
+public class DataTypeExample {
+
+	int i = 2147483647;
+	float f = 0.2f;
+	double d = 0;
+	byte b = 0;
+	char ch = '\u0000';
+	boolean flag = false;
+	short sh = 0;
+	long l = 0L;
+
+	public static void main(String[] args) {
+		byte b = (byte)129;
+		System.out.println(b);
+	}
+
+}
