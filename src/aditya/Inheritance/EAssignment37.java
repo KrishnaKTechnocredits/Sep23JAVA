2979b07544e5183b984adb3f7a5838a1edde3e79
Aditya Kunjir <adityakunjir12@gmail.com>
2023-10-09 22:04:03 +0530

    Aditya_Assignment37

---
+++ src/aditya/Inheritance/EAssignment37.java
@@ -0,0 +1,13 @@
+package aditya.Inheritance;
+
+public class EAssignment37 extends MAssignment37{
+	int y=30;
+	int z=40;
+	
+	void m2() {
+		System.out.println("EM2");
+	}
+	void m3() {
+		System.out.println("EM3");
+	}
+}
