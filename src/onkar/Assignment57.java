549d9aa7fd58466e587bf3a48c798f33535ff0da
patilonkar18 <patilonkar18@gmail.com>
2023-10-13 20:40:12 +0530

    Onkar_Assignment57

---
+++ src/onkar/Assignment57.java
@@ -0,0 +1,34 @@
+/*WAP to print consonants in the given String and count of vowels and consonants
+String str = "technocredits";
+output : 
+Consonants in the given string are : tchncrdts
+Count of vowels in given string is : 4
+Count of consonants in given string is : 9*/
+
+package onkar;
+
+public class Assignment57 {
+	
+	void printConsonants (String input) {
+		int vowelCount = 0;
+		int consonantCount = 0;
+		String output = "";
+		
+		for (int index=0; index<input.length(); index++) {
+			char ch = input.charAt(index);
+			if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+				vowelCount++;
+			}else {
+				output += ch;
+				consonantCount++;
+			}
+		}
+		System.out.println("Consonants in the given string are : "+output);
+		System.out.println("Count of vowels in given string is : "+vowelCount);
+		System.out.println("Count of consonants in given string is : "+consonantCount);
+	}
+	
+	public static void main (String[] args) {
+		new Assignment57().printConsonants("technocredits");
+	}
+}
