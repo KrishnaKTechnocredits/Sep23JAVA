dc2bf66cc1467f5b7a1e0b0551364bfa4c8b51e1
patilonkar18 <patilonkar18@gmail.com>
2023-10-13 20:27:23 +0530

    Onkar_Assignment58

---
+++ src/onkar/Assignment58.java
@@ -0,0 +1,30 @@
+/*WAP to print only special characters from the given string.
+String str = "Te_23!cHNo#3cR3 2ed*&i2t%1S$";
+output : "_!# *&%$" 
+Note: including space in between*/
+
+package onkar;
+
+public class Assignment58 {
+	
+	void printSpecialCharacters (String input) {
+		input = input.toLowerCase();
+		String output = "";
+		for (int index=0; index<input.length(); index++) {
+			char ch = input.charAt(index);
+			if (Character.isDigit(ch) || Character.isLetter(ch)) {
+				input = input.replace(ch, '?');
+			}
+		}
+		for (int i=0; i<input.length(); i++) {
+			char ch = input.charAt(i);
+			if (ch != '?')
+				output += ch;
+		}
+		System.out.println(output);
+	}
+	
+	public static void main(String[] args) {
+		new Assignment58().printSpecialCharacters("Te_23!cHNo#3cR3 2ed*&i2t%1S$");
+	}
+}
