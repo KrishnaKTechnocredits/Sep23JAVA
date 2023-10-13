0fb92e54100866b06d317bc21962810bbc7495af
Shrutika Benakatti <shrutikad905@gmail.com>
2023-10-13 18:16:39 +0530

    Shrutika_Assignment_51

---
+++ src/shrutika/StringVowelA51.java
@@ -0,0 +1,23 @@
+/*Assignment-51 : 13th Oct'2023
+String str = "technocredits";
+output : eoei */
+package shrutika;
+
+public class StringVowelA51 {
+
+	void getVowel(String input) {
+		String str = "";
+		for (int index = 0; index < input.length(); index++) {
+			char ch = input.charAt(index);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				str = str + ch;
+			}
+		}
+		System.out.println(str);
+	}
+
+	public static void main(String[] args) {
+		StringVowelA51 arrayVowelA51 = new StringVowelA51();
+		arrayVowelA51.getVowel("technocredits");
+	}
+}
