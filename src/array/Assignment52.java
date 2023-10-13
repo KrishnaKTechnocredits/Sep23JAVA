99c6c03881122b404704a744663a9b10dd0af9a1
akshitakadu24 <akshita.kadu24@gmail.com>
2023-10-13 21:35:14 +0530

    Akshita_Assignment_52

---
+++ src/array/Assignment52.java
@@ -0,0 +1,28 @@
+//WAP to print count of vowels
+//Input: technocredits
+//Output : 4
+
+
+package array;
+
+public class Assignment52 {
+
+	void countOfVowels(String input) {
+	int count=0;
+	for(int index=0;index<input.length();index++) {
+		char ch = input.charAt(index);
+		if(ch =='a' || ch =='e' || ch =='i' || ch =='o' || ch =='u') {
+			count++;
+		}
+	}
+	System.out.println("Count of Vowels : " + count);
+	}
+	
+	public static void main(String[] args) {
+		Assignment52 ass52 = new Assignment52();
+		String input = "technocredits";
+		System.out.println("Input : " + input);
+		ass52.countOfVowels(input);
+	}
+
+}
