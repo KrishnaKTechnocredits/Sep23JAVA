aba8cb1e64452f1aa95268acc4b03dfb33edc5ff
akshitakadu24 <akshita.kadu24@gmail.com>
2023-10-13 20:45:13 +0530

    indentation

--- src/akshita/String/Assignment51.java
+++ src/akshita/String/Assignment51.java
@@ -2,28 +2,25 @@
 //Input: "technocredits"
 //Output: eoei
 
-
 package akshita.String;
 
 public class Assignment51 {
 
 	void vowels(String input) {
-		String output="";
-		for(int index=0;index<input.length();index++) {
+		String output = "";
+		for (int index = 0; index < input.length(); index++) {
 			char ch = input.charAt(index);
-			if(ch =='a'||ch =='e'||ch=='i'||ch=='o' || ch =='u') {
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
 				output = output + ch;
 			}
 		}
 		System.out.println("Output : " + output);
 	}
-	
+
 	public static void main(String[] args) {
 		Assignment51 ass51 = new Assignment51();
 		String input = "technocredits";
 		System.out.println("input : " + input);
 		ass51.vowels(input);
-
 	}
-
 }
