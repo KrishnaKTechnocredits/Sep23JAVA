e7ae6408532ef1fc22a31ba7310621b01d7b324d
Hetalvirani06 <hetalusadadiya@gmail.com>
2023-10-15 18:28:05 +0530

    Hetal_Assignment25

---
+++ src/hetal/Assignment25.java
@@ -0,0 +1,35 @@
+//*Print the freq of all character using nested for loop.
+//*	String str = "aakansha"
+	//*output : a -> 4
+        //* k -> 2
+		//* n -> 1 
+		 //*s -> 1
+        //* h -> 1 *//
+
+package hetal;
+
+public class Assignment25 {
+	
+	void printCharFreq(String input){
+		for(int index=0;index<input.length();index++){
+		int count = 0;
+		char ch = input.charAt(index);
+		if(ch != '*'){
+			for(int innerIndex=0;innerIndex<input.length();innerIndex++){
+				if(ch==input.charAt(innerIndex))
+					count++;
+			}
+			System.out.println(ch + "->" + count);
+			input = input.replace(ch,'*');
+		}
+		}
+	}
+	
+	public static void main(String[] args){
+		Assignment25 assignment25 = new Assignment25();
+		assignment25.printCharFreq("aakanksha");
+	}
+		
+}
+
+
