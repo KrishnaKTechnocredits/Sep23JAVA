199fa715feb2fb90b03880301c0fdda0ecc2b59f
Shrutika Benakatti <shrutikad905@gmail.com>
2023-10-16 22:51:51 +0530

    Shrutika_Assignment_56

---
+++ src/shrutika/ArryA56.java
@@ -0,0 +1,36 @@
+/*Assignment-56 : 13th Oct'2023 Print vowels of longest string in an array
+
+String[] str = {"technocredits","aakanksha","yogesh","shamli"}; 
+output : technocredits -> eoei     
+   */
+package shrutika;
+
+public class ArryA56 {
+	String str = "";
+
+	String vowel(String input) {
+		for (int index = 0; index < input.length(); index++) {
+			char ch = input.charAt(index);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				str = str + ch;
+			}
+		}
+		return str;
+	}
+
+	void longestString(String[] arry) {
+		String maxLength = "";
+		for (int index = 0; index < arry.length; index++) {
+			if (arry[index].length() > maxLength.length()) {
+				maxLength = arry[index];
+			}
+		}
+		str = vowel(maxLength);
+		System.out.println(maxLength + " -> " + str);
+	}
+
+	public static void main(String[] args) {
+		String[] input = { "technocredits", "aakanksha", "yogesh", "shamli" };
+		new ArryA56().longestString(input);
+	}
+}
