e910e896d3e5c485cdae633aa776445c9cba0c40
Shrutika Benakatti <shrutikad905@gmail.com>
2023-10-23 07:47:55 +0530

    shrutika_Assignment_59

--- src/shrutika/As59ScannerCalculator.java
+++ src/shrutika/As59ScannerCalculator.java
@@ -1,3 +1,16 @@
+/*Assignment - 59 : 21st Oct'2023
+
+Using scanner class, take 2 inputs from user and ask user which operation they would like to perform , accordingly call methods of calculator.
+
+input : 
+enter input 1 : 23
+enter input 2 : 20
+
+which operation you want to perform, valid operations are [add,sub,mul,div].
+add
+
+output : sum of 23 and 20 is 43 */
+
 package shrutika;
 
 import java.util.Scanner;
