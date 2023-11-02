7ceb38b40f7b67653075b0d63e39976d6b77a4b0
patilonkar18 <patilonkar18@gmail.com>
2023-11-02 18:59:37 +0530

    Onkar_Assignment70

---
+++ src/onkar/Assignment70.java
@@ -0,0 +1,39 @@
+/*Write a method to print unique names from given ArrayList.
+
+void getUniqueNameList(ArrayList<String> listOfNames){
+	// Here add your logic
+	
+} */
+
+package onkar;
+
+import java.util.ArrayList;
+
+public class Assignment70 {
+	
+	void getUniqueNameList(ArrayList<String> listOfNames){
+		System.out.println("Below are the unique elements of the given ArrayList :");
+		for (int index=0; index<listOfNames.size(); index++) {
+			String input = listOfNames.get(index);
+			if(listOfNames.indexOf(input) == listOfNames.lastIndexOf(input))
+				System.out.println(input);
+		}
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<String> a1 = new ArrayList<String>();
+		a1.add("Onkar");
+		a1.add("jay");
+		a1.add("narendra");
+		a1.add("rahul");
+		a1.add("Onkar");
+		a1.add("swati");
+		a1.add("sakshi");
+		a1.add("rahul");
+		a1.add("hetal");
+		a1.add("jay");
+		a1.add("denish");
+		a1.add("rahul");
+		new Assignment70().getUniqueNameList(a1);
+	}
+}
