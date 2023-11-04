06df70410908254a3ba6d1a0fbef372522753e29
Swati Kudale <swatikudale92@gmail.com>
2023-11-04 14:25:28 +0530

    Swati_Assignment_81

--- src/swati/Ass81ReturnUniqueSetOfNumbers.java
+++ src/swati/Ass81ReturnUniqueSetOfNumbers.java
@@ -10,17 +10,18 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 public class Ass81ReturnUniqueSetOfNumbers {
-	
-	void printUniqueSetOFNumbers(LinkedHashSet<Integer> input){
-		for(int num : input){
+
+	void printUniqueSetOFNumbers(LinkedHashSet<Integer> input) {
+		for (int num : input) {
 			input.add(num);
 		}
-		System.out.println("unique set of number from all given numbers : "+input);
+		System.out.println("unique set of number from all given numbers : " + input);
 	}
 
 	public static void main(String[] args) {
 		Ass81ReturnUniqueSetOfNumbers ass81 = new Ass81ReturnUniqueSetOfNumbers();
-		LinkedHashSet<Integer> setOfNumbers = new LinkedHashSet<Integer>(Arrays.asList(10,11,10,44,55,4,55,66,67));
+		LinkedHashSet<Integer> setOfNumbers = new LinkedHashSet<Integer>(
+				Arrays.asList(10, 11, 10, 44, 55, 4, 55, 66, 67));
 		ass81.printUniqueSetOFNumbers(setOfNumbers);
 	}
 }
