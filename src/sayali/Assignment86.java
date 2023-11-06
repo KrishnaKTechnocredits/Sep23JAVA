a093615d6bca0315952d2a62674e4d62f223619d
Sayali Honrao <sayali.honrao@gmail.com>
2023-11-06 22:00:32 +0530

    Sayali_Assignment_86

--- src/sayali/Assignment86.java
+++ src/sayali/Assignment86.java
@@ -10,42 +10,43 @@ List<String> getUniqueWord(Map<String, Integer> wordMap){
 
 package sayali;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Assignment86 {
 
-	Map<String, Integer> getUniqueWordsFrequencyMap(String input) {
-		String[] arr = input.split(" ");
-		System.out.println("Input : " + input);
-		Map<String, Integer> output = new LinkedHashMap<>();
-		for (String str : arr) {
-			if (output.containsKey(str))
-				output.put(str, output.get(str) + 1);
-			else
-				output.put(str, 1);
-		}
-		return output;
-	}
+	Map<String, Integer> getUniqueWords(String str) {
+		String[] arr = str.split(" ");
+		Map<String, Integer> wordFreq = new LinkedHashMap<String, Integer>();
 
-	List<String> getUniqueWords(Map<String, Integer> wordMap) {
-		Set<String> keys = wordMap.keySet();
-		List<String> output1 = new ArrayList<>();
-		for (String str : keys) {
-			if (wordMap.get(str) == 1) {
-				output1.add(str);
+		for (String name : arr) {
+			if (wordFreq.containsKey(name)) {
+				int count = wordFreq.get(name);
+				wordFreq.put(name, count + 1);
+			} else {
+				wordFreq.put(name, 1);
 			}
 		}
-		return output1;
+		return wordFreq;
+	}
+
+	void uniqueWord(Map<String, Integer> input) {
+		Set<String> word = input.keySet();
+
+		for (String output : word) {
+			int freq = input.get(output);
+			if (freq == 1) {
+				System.out.println(output + "->" + freq);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
+		String str = "hello techno techno hi credits hi techno hello techno pune";
 		Assignment86 ass86 = new Assignment86();
-		Map<String, Integer> wordMap = ass86
-				.getUniqueWordsFrequencyMap("hello techno techno hi credits hi techno hello techno pune");
-		System.out.println("Output : " + ass86.getUniqueWords(wordMap));
+		Map<String, Integer> uniqueNameAndFreq = ass86.getUniqueWords(str);
+		System.out.println("Output: ");
+		ass86.uniqueWord(uniqueNameAndFreq);
 	}
 }
